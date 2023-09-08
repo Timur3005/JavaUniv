88e8c83d60809c2159a7647c4fb3c036140d200d
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2023-09-08 17:59:07 +0300

    seventh th is done

---
+++ src/seventh/MathCalculable.java
@@ -0,0 +1,9 @@
+package seventh;
+
+public interface MathCalculable {
+
+    double PI = Math.PI;
+    double pow(double x, int n);
+    double absComplex(ComplexNumber complexNumber);
+
+}
---
+++ src/seventh/MathFunc.java
@@ -0,0 +1,26 @@
+package seventh;
+
+public class MathFunc implements MathCalculable{
+    @Override
+    public double pow(double x, int n) {
+
+        while (n>1){
+            n--;
+            x*=x;
+        }
+        return x;
+    }
+
+    @Override
+    public double absComplex(ComplexNumber complexNumber) {
+        return Math.sqrt(complexNumber.getA()*complexNumber.getA() + complexNumber.getB()*complexNumber.getB());
+    }
+
+    public double getCircleSquare(double r){
+        return pow(r, 2)*MathCalculable.PI;
+    }
+
+    public double getRectangleSquare(double a, double b){
+        return a*b;
+    }
+}
