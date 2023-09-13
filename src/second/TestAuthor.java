53413d59e047f5f1f0ba7c249335c1760c694fce
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2023-09-13 11:18:24 +0300

    2.2 is done

--- src/second/TestAuthor.java
+++ src/second/TestAuthor.java
@@ -5,8 +5,6 @@ public class TestAuthor {
     public static void main(String[] args){
         Author author = new Author("Ramil Nig", null, 'm');
         author.setEmail("ramil@mail.ru");
-        System.out.println("email "+author.getEmail());
-        System.out.println("name "+author.getName());
-        System.out.println("gender "+author.getGender());
+        System.out.println(author);
     }
 }
---
+++ src/second/TestBall.java
@@ -0,0 +1,10 @@
+package second;
+
+public class TestBall {
+
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        System.out.println(ball);
+        ball.move(1,4);
+    }
+}
